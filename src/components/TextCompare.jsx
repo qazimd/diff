75057cd9bbb45b0a8492891fc7e26b9502150868
Qazim Dervishi <qazim.dervishi003@gmail.com>
2023-05-31 23:02:02 +0200

    minor

--- src/components/TextCompare.jsx
+++ src/components/TextCompare.jsx
@@ -3,41 +3,42 @@ import "bootstrap/dist/css/bootstrap.css";
 import React from "react";
 
 function TextCompare(props) {
-    let x = (
-      <div
-        className="difference"
-        dangerouslySetInnerHTML={{ __html: props.difference }}
-      />
-    );
-  
-    return (
-      <div>
-        <div className="text-area">
-          <div>
-            <label>Original Text</label>
-            <textarea onChange={(e) => props.setText1(e.target.value)} />
-          </div>
-          <div>
-            <label>Changed Text</label>
-            <textarea onChange={(e) => props.setText2(e.target.value)} />
-          </div>
-        </div>
-  
-        <div>
-          <button className="btn btn-success" onClick={props.calculateDifference}>
-            Find Difference
-          </button>
-        </div>
-        {props.difference && (
-          <div className="difference">
-            <p>🔴- indicates deletion 🟢- indicates addition</p>
-            <p>These are the differences:</p>
-            {x}
-          </div>
-        )}
+  let x = (
+    <div
+      className="difference"
+      dangerouslySetInnerHTML={{ __html: props.difference }}
+    />
+  );
+
+  return (
+    <>
+      <div className="text-area">
+            <div>
+             <label>Original Text</label>
+             <textarea onChange={(e) => props.setText1(e.target.value)} />
+            </div>
+
+            <div>
+             <label>Changed Text</label>
+             <textarea onChange={(e) => props.setText2(e.target.value)} />  
+            </div>
       </div>
-    );
-  }
-  
+
+      <div>
+        <button className="btn btn-success" onClick={props.calculateDifference}>
+          Find Difference
+        </button>
+      </div>
+
+      {props.difference && (
+        <div className="difference">
+          <p>🔴- indicates deletion 🟢- indicates addition</p>
+          <p>These are the differences:</p>
+          {x}
+        </div>
+      )}
+    </>
+  );
+}
 
 export default TextCompare;
